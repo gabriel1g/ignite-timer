589c7db438e832982c199abc8a4868cd22a42bcc
gabrielrodrigues <gabriel.grf8395@gmail.com>
2023-04-10 13:23:58 -0300

    configuração dos alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
   plugins: [react()],
-})
+});
