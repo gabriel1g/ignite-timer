4434bfd13db03633feedfb3d03a0ca5f45a36bbf
gabrielrodrigues <gabriel.grf8395@gmail.com>
2023-04-10 14:16:30 -0300

    global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { ThemeProvider } from 'styled-components';
 
+import { GlobalStyle } from '@styles/global';
 import { defaultTheme } from '@styles/themes/default';
 
 export default function App() {
-  return <ThemeProvider theme={defaultTheme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme['gray-900']};
+    color: ${({ theme }) => theme['gray-300']}
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto';
+    font-size: 1rem;
+    font-weight: normal;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']};
+  }
+`;
