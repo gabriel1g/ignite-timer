bf8c788eca866894ebcc2ee2b282ed5ea883c617
gabrielrodrigues <gabriel.grf8395@gmail.com>
2023-04-10 15:24:46 -0300

    criação das rotas da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { BrowserRouter } from 'react-router-dom';
+
 import { ThemeProvider } from 'styled-components';
 
+import { Router } from '@routes/Router';
 import { GlobalStyle } from '@styles/global';
 import { defaultTheme } from '@styles/themes/default';
 
@@ -7,6 +10,9 @@ export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
